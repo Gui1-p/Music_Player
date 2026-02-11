e3610dddb34a2f6c04c8f690f622f8755513179e
Gui1-p <guilhermepaivaperes@gmail.com>
2026-02-11 13:52:11 -0300

    Inicio do repositório. Este repositório é destuinado ao meu estudo de html, css e javascript.

---
+++ script.js
@@ -0,0 +1,10 @@
+const Song_name = document.getElementById("song-name");
+const Band_name = document.getElementById("band-name");
+const song = document.getElementById("audio");
+
+Song_name.innerText = "One";
+Band_name.innerText = "Metallica";
+
+song.play();
+song.pause();
+
